4078935a84cb18b4db18005b5ec8446c986d7246
Raunak Kumar <kumarraunak085@gmail.com>
2025-08-04 02:59:39 +0530

    fifth commit

--- backend/routes/downloadimageroutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { downloadSavedImage } from '../controllers/imageControllers.js';
-
-const router = express.Router();
-
-router.get('/api/download/:fileName', downloadSavedImage);
-
-export default router;
